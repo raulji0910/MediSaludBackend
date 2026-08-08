d81bf508e2c00be7c41ecbf5ba9ccb614b398f6e
raulji0910 <majive2006@hotmail.com>
2026-08-08 13:47:35 -0500

    chore: initial Spring Boot project setup
    Java 17 + Spring Boot 3.3.2, Maven, H2 en memoria, Flyway y
    springdoc-openapi (Swagger). Incluye el enunciado de la prueba tecnica.

---
+++ src/main/java/com/ceiba/medisalud/MediSaludApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.medisalud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediSaludApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediSaludApplication.class, args);
+    }
+
+}
